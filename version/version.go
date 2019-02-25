7b10a0b05e4ce96c67a35a8ddac5456333130f3a
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2019-02-25 15:48:24 +0800

    version: CompareVersion and test cases
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

--- version/version.go
+++ version/version.go
@@ -1,5 +1,19 @@
 package version
 
+import (
+	"bytes"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/jiangxin/multi-log"
+)
+
+// Macros for version package
+const (
+	MinGitVersion = "1.7.2"
+)
+
 var (
 	// Version of git-repo
 	Version = "undefined"
@@ -8,4 +22,68 @@ var (
 // GetVersion show git-repo version
 func GetVersion() string {
 	return Version
+}
+
+// GetGitVersion gets current installed git version
+func GetGitVersion() string {
+	var out bytes.Buffer
+
+	cmd := exec.Command("git", "version")
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Errorf("fail to run git version")
+		return ""
+	}
+	return strings.TrimSpace(strings.TrimPrefix(out.String(), "git version "))
+}
+
+// CompareVersion compares two versions
+func CompareVersion(_left, _right string) int {
+	left := strings.Split(_left, ".")
+	right := strings.Split(_right, ".")
+	pos := len(left)
+	if pos > len(right) {
+		pos = len(right)
+	}
+
+	for i := 0; i < pos; i++ {
+		l, err := strconv.Atoi(left[i])
+		if err != nil {
+			if left[i] > right[i] {
+				return 1
+			} else if left[i] < right[i] {
+				return -1
+			} else {
+				continue
+			}
+		}
+
+		r, err := strconv.Atoi(right[i])
+		if err != nil {
+			return -1
+		}
+		if l > r {
+			return 1
+		} else if l < r {
+			return -1
+		}
+	}
+
+	if len(left) == len(right) {
+		return 0
+	}
+
+	if len(left) > len(right) {
+		if _, err := strconv.Atoi(left[pos]); err == nil {
+			return 1
+		}
+		return -1
+	}
+
+	if _, err := strconv.Atoi(right[pos]); err == nil {
+		return -1
+	}
+
+	return 1
 }
---
+++ version/version_test.go
@@ -0,0 +1,17 @@
+package version
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompareVersion(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(0, CompareVersion("10", "10"))
+	assert.Equal(1, CompareVersion("11", "10"))
+	assert.Equal(-1, CompareVersion("10", "11"))
+	assert.Equal(-1, CompareVersion("2.2.3", "2.10.1"))
+	assert.Equal(-1, CompareVersion("1.2.3", "1.2.3.4"))
+	assert.Equal(1, CompareVersion("1.2.3", "1.2.3.rc1"))
+}
