ae22986f1a0e25123b50c5a3cea4441c840cf620
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2020-11-17 09:26:37 +0800

    refactor: create gitdir to isolate workspace for test
    Create toplevel gitdir in trash-dir order to prevent dangous operation
    on current repo.
    
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

--- test/sharness.d/repo-ceiling.sh
+++ test/sharness.d/repo-ceiling.sh
@@ -2,3 +2,6 @@
 
 # Setup a ".repo" file to stop search for .repo dir upward.
 touch "${SHARNESS_TEST_SRCDIR}/.repo"
+
+# Create toplevel gitdir to prevent dangous operation on current repo.
+git init "$SHARNESS_TRASH_DIRECTORY"
