106f09b49c3b69be7a9907c5a38e044ed78c541c
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2021-08-03 10:26:12 +0800

    sshinfo: check response of ssh_info for bad hostname
    Try to validate response of ssh_info, and show error if hostname or
    username has invalid characters.
    
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

--- helper/ssh-info_test.go
+++ helper/ssh-info_test.go
@@ -59,3 +59,75 @@ func TestGetReviewRef(t *testing.T) {
 	assert.Nil(err)
 	assert.Equal("refs/{pull}/123/head", ref, "unknown key")
 }
+
+func TestSSHInfoFromString(t *testing.T) {
+	assert := assert.New(t)
+
+	for _, data := range []struct {
+		SSHInfo string
+		ErrMsg  string
+	}{
+		{
+			SSHInfo: "",
+			ErrMsg:  "empty ssh_info",
+		},
+		{
+			SSHInfo: `
+		<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+		<html>
+		<head><title>302 Found</title></head>
+		<body bgcolor="white">
+		<h1>302 Found</h1>
+		<p>The requested resource resides temporarily under a different URI.</p>
+		<hr/>Powered by Tengine</body>
+		</html>`,
+			ErrMsg: "ssh_info returns a normal HTML response",
+		},
+		{
+			SSHInfo: `NOT_AVAILABLE`,
+			ErrMsg:  "",
+		},
+		{
+			SSHInfo: `host.name 22`,
+			ErrMsg:  "",
+		},
+		{
+			SSHInfo: `bad host-name 22`,
+			ErrMsg:  "invalid ssh_info response: invalid character 'b' looking for beginning of value",
+		},
+		{
+			SSHInfo: `{"host":"codeup.aliyun.com","port":22,"type":"agit"}`,
+			ErrMsg:  "",
+		},
+		{
+			SSHInfo: `{
+					"host":"codeup.aliyun.com",
+					"port":22,"type":"agit"
+				  }`,
+			ErrMsg: "",
+		},
+		{
+			SSHInfo: `{"host":"codeup.aliyun.com 22","port":22,"type":"agit"}`,
+			ErrMsg:  "ssh_info has invalid host name: codeup.aliyun.com 22",
+		},
+		{
+			SSHInfo: `{
+					"host":"codeup.aliyun.com",
+					"port":22,
+					"user":"!me",
+					"type":"agit"
+				   }`,
+			ErrMsg: "ssh_info has invalid user name: !me",
+		},
+	} {
+		sshInfo, err := sshInfoFromString(data.SSHInfo)
+		if data.ErrMsg == "" {
+			assert.NotNil(sshInfo, "nil ssh_info for: %s", data.SSHInfo)
+			assert.Nil(err, "error is not nil for: %s", data.SSHInfo)
+		} else {
+			assert.Nil(sshInfo, "ssh_info is not nil for: %s", data.SSHInfo)
+			assert.NotNil(err, "error is nil for: %s", data.SSHInfo)
+			assert.Equal(data.ErrMsg, err.Error(), "unmatch error msg for: %s", data.SSHInfo)
+		}
+	}
+}
