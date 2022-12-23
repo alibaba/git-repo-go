243218e6fab2b18c42e51f154b2fd1db13ad2d95
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2022-12-23 15:16:11 +0800

    test: set default branch for git-init
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

--- test/sharness.d/branch-name.sh
+++ test/sharness.d/branch-name.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+: ${GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME:=master}
+export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
+
 git_current_branch() {
 	branch=$(git symbolic-ref HEAD 2>/dev/null | sed -e "s#refs/heads/##") &&
 	if test -n "$branch"
